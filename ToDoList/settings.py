c6fe9a36b26a637406e485b81b57cc264085890a
Yancarlo Ospina Arias <yancarloospinaarias@MacBook-Air-de-Yancarlo.local>
2025-04-03 20:20:29 -0500

    Elimine error del login, se repetia

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -82,11 +82,12 @@ WSGI_APPLICATION = 'ToDoList.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://postgres:postgres@localhost/postgres',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
+
 
 
 # Password validation
